ab0803db2fb3afc0e2ee6ffa17cfa4865094352e
Marcos Ferreira <ferreira2006@gmail.com>
2025-08-12 07:48:20 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -398,6 +398,7 @@ const App = {
     Storage.saveFavorites(favorites);
     UI.renderFavorites();
     UI.showToast(`"${city}" adicionado aos favoritos!`);
+    console.log();
     this.updateFavBtnState();
   },
 
