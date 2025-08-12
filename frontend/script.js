86e607ecc49c8c359705f1d8e7e8b9348e20f42e
Marcos Ferreira <ferreira2006@gmail.com>
2025-08-12 16:03:49 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -104,10 +104,13 @@ const UI = {
   },
 
   showToast(message, duration = 3000) {
-    const t = dom.toast;
-    t.textContent = message;
-    t.classList.add("show");
-    setTimeout(() => t.classList.remove("show"), duration);
+   const t = dom.toast;
+  t.textContent = message;
+  t.classList.remove("show");
+  void t.offsetWidth; // forçar reflow
+  t.classList.add("show");
+  dom.cityInput.focus();
+  setTimeout(() => t.classList.remove("show"), duration);
   },
 
   setDynamicBackground(mainWeather) {
