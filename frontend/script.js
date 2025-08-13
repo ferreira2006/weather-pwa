a5481d7ff6a81bf0e7752b6d33fd0d55bac453a3
Marcos Ferreira <ferreira2006@gmail.com>
2025-08-13 14:16:26 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -275,16 +275,16 @@ const UI = {
 
   applySavedTheme() {
     const saved = Storage.getTheme();
-    if (saved === "dark") {
-      document.body.classList.add("dark");
-      document.body.classList.remove("light");
-    } else {
-      document.body.classList.add("light");
-      document.body.classList.remove("dark");
-    }
-    this.updateThemeColors();
-    this.updateThemeToggleButton();
-    this.setDynamicBackgroundFromCurrentIcon();
+  if (saved === "dark") {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+  this.updateThemeColors();
+  this.updateThemeToggleButton();
+  this.setDynamicBackgroundFromCurrentIcon();
   }
 };
 
