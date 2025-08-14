2e422c1524c09620588578b3fa71e39c244e9594
Marcos Ferreira <ferreira2006@gmail.com>
2025-08-14 17:28:09 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,16 @@
 
+const backendUrl = "https://weather-backend-hh3w.onrender.com/weather";
+
+export const WeatherAPI = {
+  async fetchByCity(city) {
+    const res = await fetch(`${backendUrl}?city=${encodeURIComponent(city)}&days=1`);
+    if (!res.ok) throw new Error("Cidade não encontrada");
+    return res.json();
+  },
+
+  async fetchByCoords(lat, lon) {
+    const res = await fetch(`${backendUrl}?lat=${lat}&lon=${lon}&days=1`);
+    if (!res.ok) throw new Error("Não foi possível obter o clima para sua localização.");
+    return res.json();
+  }
+};
