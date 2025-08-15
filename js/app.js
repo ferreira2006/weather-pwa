b0fd07f4e9f4ea3945474e983f484596868beaa3
Marcos Ferreira <ferreira2006@gmail.com>
2025-08-15 10:55:30 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -48,14 +48,24 @@ let firstLoad = true;
 // ===== API =====
 const WeatherAPI = {
   async fetchByCity(city) {
-    const res = await fetch(`${backendUrl}?city=${encodeURIComponent(city)}&days=1`);
-    if (!res.ok) throw new Error("Cidade não encontrada");
-    return res.json();
+    try {
+      const res = await fetch(`${backendUrl}?city=${encodeURIComponent(city)}&days=1`);
+      if (!res.ok) throw new Error("Cidade não encontrada");
+      return res.json();
+    } catch (err) {
+      if (err instanceof TypeError) throw new Error("Erro de conexão. Verifique sua internet.");
+      else throw err;
+    }
   },
   async fetchByCoords(lat, lon) {
-    const res = await fetch(`${backendUrl}?lat=${lat}&lon=${lon}&days=1`);
-    if (!res.ok) throw new Error("Não foi possível obter o clima para sua localização.");
-    return res.json();
+    try {
+      const res = await fetch(`${backendUrl}?lat=${lat}&lon=${lon}&days=1`);
+      if (!res.ok) throw new Error("Não foi possível obter o clima para sua localização.");
+      return res.json();
+    } catch (err) {
+      if (err instanceof TypeError) throw new Error("Erro de conexão. Verifique sua internet.");
+      else throw err;
+    }
   }
 };
 
